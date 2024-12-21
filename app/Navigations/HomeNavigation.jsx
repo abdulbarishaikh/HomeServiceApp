5b16715bbedee719c4ceec4af06ec5efd6e5058d
abdulbari <abdulbarishaikh279@gmail.com>
2024-12-21 19:07:22 +0530

    removed unused code

--- app/Navigations/HomeNavigation.jsx
+++ app/Navigations/HomeNavigation.jsx
@@ -1,10 +1,8 @@
-import { View, Text } from 'react-native'
 import React from 'react'
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './../Screens/HomeScreen/HomeScreen'
 import BusinessListByCategoryScreen from '../Screens/BusinessListByCategoryScreen/BusinessListByCategoryScreen';
 import BusinessDetailScreen from '../Screens/BusinessDetailScreen/BusinessDetailScreen';
-import { useAuth } from '@clerk/clerk-expo';
 
 
 const Stack = createStackNavigator();
